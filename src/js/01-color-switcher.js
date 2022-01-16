b04180e89a601fbff8706aea2f28a3907395e888
Denis Serkhel <ripvanfish27@gmail.com>
2022-01-16 16:07:35 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+    startBtn: document.querySelector("button[data-start]"),
+    stopBtn: document.querySelector("button[data-stop]"),
+    
+}
 
+
+console.log(refs.startBtn);
+console.log(refs.stopBtn);
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener("click", onClickStartBtn);
+refs.stopBtn.addEventListener("click", onClickStopBtn);
+
+function onClickStartBtn() {
+    refs.startBtn.classList.add("js_disabled");
+    body.
+}
+function onClickStopBtn() {
+    refs.startBtn.classList.remove("js_disabled");
+}
